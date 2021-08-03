78e2264829e035e924d2e90a54053686d2fe250b
christina <chrstnaheadley@gmail.com>
2021-08-03 12:23:58 -0400

    test git update

--- require_http.rb
+++ require_http.rb
@@ -1,3 +1,3 @@
 require "http"
-response = HTTP.get("https://mysterious-shelf-77181.herokuapp.com/api/all_movies_path")
+response = HTTPS.get("https://mysterious-shelf-77181.herokuapp.com/api/all_movies_path")
 pp response.parse
